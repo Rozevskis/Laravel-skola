3073417b5be796a1f3ced1fe9fca2de7440a5ac0
Kristofers Rozevskis <you@example.com>
2024-04-04 13:31:46 +0300

    last 04042024

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+Route::get('/', [WelcomeController::class, 'welcome']);
