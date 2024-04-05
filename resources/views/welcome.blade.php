92469cfe9b42c61069b224a9b83d171f89481a4f
Kristofers Rozevskis <you@example.com>
2024-04-05 09:38:16 +0300

    added partial and db connection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,8 @@
         />
     </head>
     <body>
-        
+        @include('partials._navbar');
+        {{-- @include --}}
     </body>
     
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [WelcomeController::class, 'welcome']);
+Route::get('/home', [HomeController::class, 'home']);
